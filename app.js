12a3e0a3a9bd49ce4ec9a680f8deead02dfa5681
GoDev-web <godev.dev@gmail.com>
2021-02-09 11:01:28 +0100

    Initialize mongoose connection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import compression from 'compression';
 import helmet from 'helmet';
@@ -7,23 +8,37 @@ import morgan from 'morgan';
 const isProduction = process.env.NODE_ENV === 'production';
 console.log(isProduction);
 
-const app = express();
+(async () => {
+  try {
+    await mongoose.connect('mongodb://localhost/godev', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: true,
+    });
+    mongoose.set('debug', true);
+    console.log('Mongo connected...');
 
-if (!isProduction) {
-  app.use(morgan('dev'));
-}
+    const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(helmet.hidePoweredBy());
-app.use(compression({}));
+    if (!isProduction) {
+      app.use(morgan('dev'));
+    }
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from backend.</h1>');
-});
+    app.use(cors());
+    app.use(express.json());
+    app.use(helmet.hidePoweredBy());
+    app.use(compression({}));
 
-const PORT = process.env.PORT || 1337;
+    app.get('/', (req, res) => {
+      res.send('<h1>Hello from backend.</h1>');
+    });
 
-app.listen(PORT, () => {
-  console.log(`App is ready on http://localhost:${PORT}`);
-});
+    const PORT = process.env.PORT || 1337;
+
+    app.listen(PORT, () => {
+      console.log(`App is ready on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
